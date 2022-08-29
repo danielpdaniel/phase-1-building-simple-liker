5346b8523db89a7c5e0e57d707b1468d362e750d
danielpdaniel <artofjquinn@gmail.com>
2022-08-29 13:46:13 -0400

    commit tree 016891ca99ec421d14f87962da252c2d7c39a128

--- main.js
+++ main.js
@@ -3,6 +3,49 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll("span.like-glyph");
+
+// for (element of hearts){
+//   element.addEventListener("click", function(event){
+//     let targetedElement = event.target
+//     console.log(targetedElement);
+//     // element.currentTarget.textContent = FULL_HEART;
+//   })
+// }
+
+// function addClick(array){
+//   for (i = 0; i < array.length; i++){
+//     array[i].addEventListener("click", function(){
+//       console.log(`i'm number ${i}`)
+//     })
+//   } 
+// }
+// addClick(hearts);
+
+for(element of hearts){
+  element.addEventListener("click", function(event){
+    let clickedHeart = event.target
+    mimicServerCall(clickedHeart)
+    .then(() => {
+      if(clickedHeart.className === "like-glyph"){
+      clickedHeart.textContent = FULL_HEART;
+      clickedHeart.className = "activated-heart";
+      } else if (clickedHeart.className === "activated-heart"){
+        clickedHeart.textContent = EMPTY_HEART;
+        clickedHeart.className = "like-glyph";
+      }
+    console.log(clickedHeart);
+  })
+    .catch(() => {
+      const errorMessage = document.getElementById("modal");
+    errorMessage.className = "hey lol";
+    setTimeout(function(){
+      errorMessage.className = "hidden";
+    }, 3000)
+    console.log(errorMessage);
+  })
+})
+}
 
 
 
